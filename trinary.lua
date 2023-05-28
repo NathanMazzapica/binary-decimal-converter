126ae4ea58148f1db96502819ae348215d1ed80a
Nathan Mazzapica <92413483+NathanMazzapica@users.noreply.github.com>
2023-05-28 11:34:37 -0400

    Added code to set up and print table.

--- trinary.lua
+++ trinary.lua
@@ -6,14 +6,23 @@
 
 -- start y from 1 and multiply by 2 until y >= x
 
-local toConvert = 16
+local toConvert = 18
 
-local binaryPlaces = {}
+local binaryPlaces = { 1 }
 
-local i = 0
+local i = 1
 
-while(i < toConvert) do
+while( i < toConvert ) do
     i = i * 2
     if ( i > toConvert ) then break end
+    table.insert( binaryPlaces, i )
 end
+
+function PrintTable( tableToPrint )
+    for k, v in ipairs( tableToPrint ) do
+        print( v )
+    end
+end
+
+PrintTable( binaryPlaces )
 
